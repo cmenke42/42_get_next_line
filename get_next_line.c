a291a34195bd69eae6121fd33c043dedc3404579
Carlos Menke <cmenke@c2s13d68.42wolfsburg.de>
2023-01-07 16:02:06 +0100

    UPDATE

--- get_next_line.c
+++ get_next_line.c
@@ -6,21 +6,26 @@
 /*   By: cmenke <cmenke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 21:47:28 by cmenke            #+#    #+#             */
-/*   Updated: 2023/01/06 22:23:33 by cmenke           ###   ########.fr       */
+/*   Updated: 2023/01/07 16:01:43 by cmenke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-void *get_line(char **remainder, char **result)
-{
+// compare the buffer for \0 or \n
 
-}
+// -----------
+//check the remainder
+
+//read till NULL or \n
+
+//check buffer for /n or null
+
+//create the remainder
 
 char	*get_next_line(int fd)
 {
 	static char *remainder;
-	char *result;
 	
 	if (fd == -1)
 		return (NULL);
@@ -35,6 +40,6 @@ int main(void)
 	int fd;
 	
 	fd = open("lines.txt", O_RDONLY);
-	printf("%s\n", get_next_line(fd));
+	printf("%s", get_next_line(fd));
 	close(fd);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,25 @@
 /*   By: cmenke <cmenke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/06 21:47:31 by cmenke            #+#    #+#             */
-/*   Updated: 2023/01/06 21:47:32 by cmenke           ###   ########.fr       */
+/*   Updated: 2023/01/07 16:01:46 by cmenke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
 
-#include <stdlib.h>
-#include <unistd.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/uio.h>
 
 char	*get_next_line(int fd);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
 
 #endif
