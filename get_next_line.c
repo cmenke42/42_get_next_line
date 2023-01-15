c053176f31e1f30e90613822c01ac4347221ce89
cmenke <cmenke@student.42wolfsburg.de>
2023-01-15 16:58:52 +0000

    UPDATE

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,20 @@
 /*   By: cmenke <cmenke@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/15 11:52:31 by cmenke            #+#    #+#             */
-/*   Updated: 2023/01/15 16:40:51 by cmenke           ###   ########.fr       */
+/*   Updated: 2023/01/15 16:58:38 by cmenke           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+//reads the file for BUFFER_SIZE and returns NULL on error.
 char	*ft_read_line(int fd, int *inf)
 {
 	char	*buffer;
 	int		read_ret;
 
 	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
-	if (buffer == NULL)
+	if (!buffer)
 		return (NULL);
 	read_ret = read(fd, buffer, BUFFER_SIZE);
 	if (read_ret == -1)
@@ -31,6 +32,7 @@ char	*ft_read_line(int fd, int *inf)
 	return (buffer);
 }
 
+//returns the line for either '\0' or '\n'.
 char	*ft_return_line(char **remainder, int *inf, char *spot)
 {
 	char	*line;
@@ -41,21 +43,22 @@ char	*ft_return_line(char **remainder, int *inf, char *spot)
 	if (*remainder && *inf == 0)
 		spot = ft_strchr(*remainder, '\0');
 	line = ft_substr(*remainder, 0, spot - *remainder + *inf);
-	if (line != NULL && *inf == 1)
+	if (line && *inf == 1)
 		temp = ft_strjoin(spot + *inf, "");
 	free(*remainder);
 	*remainder = temp;
 	return (line);
 }
 
+//checks the remainder for '\n' and if not found calls read.
+//if a line was read it will be combined with the remainder and checked again.
+//if EOF is reached the return line function gets called to check for '\0'.
 char	*ft_check_remainder(char **remainder, int fd, int *inf)
 {
 	char	*line;
 	char	*temp;
 	char	*spot;
 
-	line = NULL;
-	temp = NULL;
 	spot = NULL;
 	if (*remainder && *inf == 1)
 		spot = ft_strchr(*remainder, '\n');
@@ -77,6 +80,8 @@ char	*ft_check_remainder(char **remainder, int fd, int *inf)
 	return (line);
 }
 
+//checks if the remainder is empty and if so it will read a line.
+//then calls check remainder and frees remainder in case of error.
 char	*get_next_line(int fd)
 {
 	static char	*remainder;
@@ -87,7 +92,7 @@ char	*get_next_line(int fd)
 	line = NULL;
 	if (fd == -1)
 		return (NULL);
-	if (remainder == NULL)
+	if (!remainder)
 		remainder = ft_read_line(fd, &inf);
 	if (remainder)
 		line = ft_check_remainder(&remainder, fd, &inf);
